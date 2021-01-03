7cc902eb84b8624fc6937832cab3602d3e2a5a77
DeadPizza <alesha_r_2003@mail.ru>
2021-01-03 13:50:59 +0300

    testing testing testing...

---
+++ testing/gameplay_test.py
@@ -0,0 +1,79 @@
+import pygame
+from test import load_image
+
+# Комната занимает 1/3 экрана т.е 1280 / 3 = 430 по ширине
+#                  2/3 т.е 720 / 3 * 2 = 480 (420) по высоте, 240 (300) - на инвентарь
+
+class Map(pygame.sprite.Sprite):
+    def __init__(self, group, screen):
+        super().__init__(group)
+        self.screen = screen
+        self.img = load_image('testmap.png')
+        self.dx = 0
+        self.moving = False
+
+    def update(self, *event):
+        if event:
+            if event[0].type == pygame.MOUSEBUTTONDOWN:
+                self.moving = True
+            elif event[0].type == pygame.MOUSEBUTTONUP:
+                self.moving = False
+
+        if self.moving:
+            self.dx -= 3
+            was = True
+
+        self.screen.blit(self.img, (self.dx, 0))
+
+
+class Inventory(pygame.sprite.Sprite):
+    def __init__(self, group, screen):
+        super().__init__(group)
+        self.screen = screen
+        self.img = load_image('inventory_test.png')
+        
+    def update(self, *event):
+        self.screen.blit(self.img, (0, 420))
+
+class Hero(pygame.sprite.Sprite):
+    def __init__(self, group, number, screen):
+        super().__init__(group)
+        self.screen = screen
+        self.img = load_image('hero.png')
+        self.x = 10 + 120 * (number - 1)
+
+    def update(self, *event):
+        self.screen.blit(self.img, (self.x, 150))
+
+if __name__ == '__main__':
+    from settings import *
+    pygame.init()
+    clock = pygame.time.Clock()
+    size = WIDTH, HEIGHT
+    screen = pygame.display.set_mode(size)
+
+
+    font = pygame.font.SysFont("Arial", 18)
+    def update_fps():
+        fps = str(int(clock.get_fps()))
+        fps_text = font.render(fps, 1, pygame.Color("coral"))
+        return fps_text
+
+    target_sprite_group = pygame.sprite.Group()
+    Map(target_sprite_group, screen)
+    Hero(target_sprite_group, 1, screen)
+    Hero(target_sprite_group, 2, screen)
+    Hero(target_sprite_group, 3, screen)
+    Hero(target_sprite_group, 4, screen)
+    Inventory(target_sprite_group, screen)
+    while True:
+        events = pygame.event.get()
+        for event in events:
+            if event.type == pygame.QUIT:
+                exit()
+            target_sprite_group.update(event)
+        if not events:
+            target_sprite_group.update()
+        screen.blit(update_fps(), (10,0))
+        pygame.display.flip()
+        clock.tick(FPS)
--- testing/main_test.py
+++ testing/main_test.py
@@ -1,6 +1,7 @@
 import pygame
 from test import load_image
 from settings import *
+from gameplay_test import Inventory, Map, Hero
 
 
 pygame.init()
@@ -12,16 +13,25 @@ def set_game_stage(stage):
     global target_sprite_group
     target_sprite_group = pygame.sprite.Group()
     if stage == 'menu':
-        MainMenuPicture(target_sprite_group)
-        StartGame(target_sprite_group)
-        Cursor(target_sprite_group)
+        MainMenuPicture(target_sprite_group, screen)
+        StartGame(target_sprite_group, screen)
+        Cursor(target_sprite_group, screen)
     elif stage == 'test':
-        Shit(target_sprite_group)
-        Cursor(target_sprite_group)
+        Shit(target_sprite_group, screen)
+        Cursor(target_sprite_group, screen)
+    elif stage == 'battle':
+        Map(target_sprite_group, screen)
+        Hero(target_sprite_group, 1, screen)
+        Hero(target_sprite_group, 2, screen)
+        Hero(target_sprite_group, 3, screen)
+        Hero(target_sprite_group, 4, screen)
+        Inventory(target_sprite_group, screen)
+        Cursor(target_sprite_group, screen)
 
 class Cursor(pygame.sprite.Sprite):
-    def __init__(self, *group):
-        super().__init__(*group)
+    def __init__(self, group, screen):
+        super().__init__(group)
+        self.screen = screen
         pygame.mouse.set_visible(False)
         self.img = load_image('arrow.png')
         self.rect = self.img.get_rect()
@@ -29,23 +39,25 @@ class Cursor(pygame.sprite.Sprite):
     def update(self, *event):
         if pygame.mouse.get_focused():
             self.rect.center = pygame.mouse.get_pos()
-            screen.blit(self.img, self.rect)
+            self.screen.blit(self.img, self.rect)
 
 
 class MainMenuPicture(pygame.sprite.Sprite):
-    def __init__(self, *group):
-        super().__init__(*group)
+    def __init__(self, group, screen):
+        super().__init__(group)
+        self.screen = screen
         self.music = pygame.mixer.Sound('resources/audio/mm_music.mp3')
         self.img = load_image('bg_main_menu.png')
         self.music.play()
 
     def update(self, *event):
-        screen.blit(self.img, (0, 0))
+        self.screen.blit(self.img, (0, 0))
 
 
 class StartGame(pygame.sprite.Sprite):
-    def __init__(self, *group):
-        super().__init__(*group)
+    def __init__(self, group, screen):
+        super().__init__(group)
+        self.screen = screen
         self.img_idle = load_image('button_main_menu_start_idle.png')
         self.img_hovered = load_image('button_main_menu_start_hovered.png')
         self.rect = self.img_idle.get_rect()
@@ -56,30 +68,41 @@ class StartGame(pygame.sprite.Sprite):
         self.rect.center = 640, 550
         if self.rect.collidepoint(pygame.mouse.get_pos()):
             self.image = self.img_hovered
-            if event[0].type == pygame.MOUSEBUTTONDOWN:
-                set_game_stage('test')
+            if event and event[0].type == pygame.MOUSEBUTTONDOWN:
+                set_game_stage('battle')
         else:
             self.image = self.img_idle
 
-        screen.blit(self.image, self.rect)
+        self.screen.blit(self.image, self.rect)
 
 
 class Shit(pygame.sprite.Sprite):
-    def __init__(self, *group):
-        super().__init__(*group)
+    def __init__(self, group, screen):
+        super().__init__(group)
+        self.screen = screen
         self.img = load_image('lulza.jpg')
 
     def update(self, *event):
-        screen.blit(self.img, (0, 0))
+        self.screen.blit(self.img, (0, 0))
 
 target_sprite_group = pygame.sprite.Group()
 set_game_stage('menu')
 
+font = pygame.font.SysFont("Arial", 18) 
+def update_fps():
+	fps = str(int(clock.get_fps()))
+	fps_text = font.render(fps, 1, pygame.Color("coral"))
+	return fps_text
+
 while True:
-    for event in pygame.event.get():
+    screen.fill((0, 0, 0))
+    events = pygame.event.get()
+    for event in events:
         if event.type == pygame.QUIT:
             exit()
-            
         target_sprite_group.update(event)
+    if not events:
+        target_sprite_group.update()
+    screen.blit(update_fps(), (10,0))
     pygame.display.flip()
     clock.tick(FPS)
